79c8a69776ae13bfe8dc4a44f96af256b59786ea
valentinesamuel <valentinesamuel2580@gmail.com>
2026-03-31 22:18:10 +0100

    feat: add scrolloff and foldcolumn padding

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 
 vim.opt.updatetime = 1000 -- write swap file and trigger CursorHold after 1s of inactivity
+
+-- Padding / breathing room
+vim.opt.scrolloff = 10     -- keep 10 lines above/below the cursor
+vim.opt.sidescrolloff = 10 -- keep 10 columns left/right of the cursor
+vim.opt.foldcolumn = "1"   -- 1-char left gutter for visual left padding
